ef6d57471d7a0ff181a53c9147924243f3bd0282
AbMohiz <abmohiz22@gmail.com>
2023-09-20 01:46:31 +0500

    Converted all the functions Only 1 Image is extracting runtime is better

--- client_input.py
+++ client_input.py
@@ -1,168 +1,160 @@
+import csv
 import pprint
-
-import pandas as pd
-from selenium.webdriver.common.by import By
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
-
 import logging
 
 logger = logging.getLogger(__name__)
 
+# Function to scrape image links using Selenium and BeautifulSoup
+def scrape_image_links(url):
+    firefox_options = webdriver.FirefoxOptions()
+    firefox_options.add_argument('-headless')
 
-def get_brand_and_title(driver):
-    """
-    Get the brand and title of the product by first finding the brand and title element using the class name.
-    :param driver: driver with the product page loaded.
-    :return: a string containing the brand and title of the product.
-    """
+    driver = webdriver.Firefox(options=firefox_options)
+
     try:
-        brand_and_title_element = driver.find_element(By.CLASS_NAME, 'pr-new-br')
-        brand_and_title = brand_and_title_element.text
+        driver.get(url)
+
+        page_source = driver.page_source
+
+        soup = BeautifulSoup(page_source, 'html.parser')
+
+        div_elements = soup.find_all('div', class_='base-product-image')
+
+        image_links = []
+
+        for div_element in div_elements:
+            img_tag = div_element.find('img')
+            if img_tag and 'src' in img_tag.attrs:
+                image_url = img_tag['src']
+                image_links.append(image_url)
+
+        return image_links
+    finally:
+        driver.quit()
+
+def get_brand_and_title(soup):
+    """
+       Get the brand and title of the product from the BeautifulSoup object.
+       :param soup: BeautifulSoup object representing the product page.
+       :return: a string containing the brand and title of the product.
+       """
+    try:
+        brand_and_title_element = soup.find(class_='pr-new-br')
+        brand_and_title = brand_and_title_element.get_text()
         return brand_and_title
     except:
-        logger.critical(f'Error getting brand and title: {driver.current_url}')
+        logger.critical(f'Error getting brand and title')
         return None
 
 
-# %%
-def get_price(driver):
+def get_price(soup):
+
     """
-    Get the price of the product by first finding the price element using the class name
-     and then getting the text of the element.
-    :param driver: driver with the product page loaded.
-    :return: a string containing the price of the product.
-    """
+       Get the price of the product from the BeautifulSoup object.
+       :param soup: BeautifulSoup object representing the product page.
+       :return: a string containing the price of the product.
+       """
     try:
-        price_element = driver.find_element(By.CLASS_NAME, 'prc-dsc')
-        price = price_element.text
+        price_element = soup.find(class_='prc-dsc')
+        price = price_element.get_text()
         return price
     except:
-        logger.debug(f'Price not found: {driver.current_url}')
+        logger.debug(f'Price not found')
         return None
 
 
-# %%
-def get_attributes(driver):
+def get_attributes(soup):
     """
-    Get the attributes of the product by first finding the attributes list element
-    and then splitting the text and then pairing adjacent tokens as key-value pairs.
-    :param driver: driver with the product page loaded.
-    :return: a dictionary containing the attributes of the product.
-    """
-    attributes = {}
-    try:
-        attributes_list_element = driver.find_element(By.CLASS_NAME, 'detail-attr-container')
-        for li in attributes_list_element.find_elements(By.TAG_NAME, 'li'):
-            key, value = li.text.split('\n')
-            attributes[key] = value
-    except:
-        logger.debug(f'Attributes not found: {driver.current_url}')
-        pass
-    finally:
-        return attributes
+       Get the attributes of the product from the BeautifulSoup object.
+       :param soup: BeautifulSoup object representing the product page.
+       :return: a dictionary containing the attributes of the product.
+       """
+    attr = soup.find_all('li', class_='detail-attr-item')
 
 
-def get_images(driver):
-    images = []
-    try:
-        main_image = driver.find_element(By.CLASS_NAME, 'base-product-image').find_element(By.TAG_NAME,
-                                                                                           'img').get_attribute('src')
-        images.append(main_image)
-        for other_image in driver.find_element(By.CLASS_NAME, 'styles-module_slider__o0fqa').find_elements(By.TAG_NAME,
-                                                                                                           'img'):
-            images.append(other_image.get_attribute('src'))
-    except:
-        if not images:
-            logger.debug(f'No images found: {driver.current_url}')
-        else:
-            logger.debug(f'Only main image found: {driver.current_url}')
-    finally:
-        return images
+    attributes_dict = {}
 
+    for element in attr:
+        # Find all the text within the <span> elements and remove leading/trailing whitespace
+        span_elements = element.find_all('span')
 
-def get_description(driver):
+        if len(span_elements) == 2:
+            text_values = [item.get_text(strip=True) for item in span_elements]
+
+            # Check if there are exactly two text values (key and value)
+            if len(text_values) == 2:
+                key, value = text_values
+                attributes_dict[key] = value
+
+    return attributes_dict  # Return the attributes dictionary
+
+def get_description(soup):
+
     """
-    Get the description of the product by first finding the description list element and then iterating over the list items.
-    Initialize an empty string and append the text of each list item to the string using a new line seperator.
-    :param driver: driver with the product page loaded.
-    :return: a string containing the description of the product.
-    """
+       Get the description of the product from the BeautifulSoup object.
+       :param soup: BeautifulSoup object representing the product page.
+       :return: a string containing the description of the product.
+       """
     description = None
     try:
-        description_list_element = driver.find_element(By.CLASS_NAME, 'detail-desc-list')
-        description = description_list_element.text
+        description_list_element = soup.find(class_='detail-desc-list')
+        description = description_list_element.get_text()
     except:
-        logger.debug(f'Description not found: {driver.current_url}')
-    finally:
-        return description
-
-
-import os
-
-
-def get_unique_filename(base_filename):
-    """
-    Returns a unique filename based on the given base_filename by appending
-    a count before the file extension. If the base filename does not exist, it
-    will return the base_filename.
-    """
-    # Split the filename and its extension
-    name, ext = os.path.splitext(base_filename)
-    count = 1
-    while os.path.exists(base_filename):
-        base_filename = f"{name}_{count}{ext}"
-        count += 1
-    return base_filename
-
+        logger.debug(f'Description not found')
+    return description
 
 if __name__ == '__main__':
-    print('This module will generate a csv file containing the product details')
-    print('if the url is not valid or the product is not available, an error message will be printed.')
-    print('The csv file will be saved in the same directory as this script.')
-    print('type in quit to exit the program.')
-
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--log-level=4')
-    driver = webdriver.Chrome(options=options)
+    print('This module will scrape and print product details including image links.')
+    print('If the URL is not valid or the product is not available, an error message will be printed.')
+    print('Type "quit" to exit the program.')
 
     while True:
-        url_to_scrape = input('Enter the url to scrape: ').strip()
+        url_to_scrape = input('Enter the URL to scrape: ').strip()
         try:
             if url_to_scrape.lower().strip() == 'quit':
-                driver.quit()
                 exit()
             else:
                 try:
-                    driver.get(url_to_scrape)
-                    if driver.title == 'trendyol.com':
-                        print('product is not available')
+                    response = requests.get(url_to_scrape)
+                    if response.status_code != 200:
+                        print('Error: Invalid URL or product not available')
                         continue
+
+                    soup = BeautifulSoup(response.text, 'html.parser')
+                    if soup.title.get_text() == 'trendyol.com':
+                        print('Product is not available')
+                        continue
+
+                    # Call the function to get image links
+                    image_links = scrape_image_links(url_to_scrape)
 
                     product_details = {
                         'url': url_to_scrape,
-                        'brand_and_title': get_brand_and_title(driver),
-                        'price': get_price(driver),
-                        'attributes': get_attributes(driver),
-                        'images': get_images(driver),
-                        'description': get_description(driver)
+                        'brand_and_title': get_brand_and_title(soup),
+                        'price': get_price(soup),
+                        'attributes': get_attributes(soup),
+                        'images': image_links,  # Use the fetched image links
+                        'description': get_description(soup)
                     }
 
-                    print('product details: ')
+                    print('Product details: ')
                     pprint.pprint(product_details)
-                    df = pd.DataFrame([product_details])
 
+                    # Save product details to a CSV file
+                    csv_filename = 'product_details.csv'
+                    with open(csv_filename, 'a', newline='', encoding='utf-8') as csv_file:
+                        fieldnames = ['url', 'brand_and_title', 'price', 'attributes', 'images', 'description']
+                        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                        # Write the header only once
+                        if csv_file.tell() == 0:
+                            writer.writeheader()
+                        writer.writerow(product_details)
+
+                    print(f'Product details saved to {csv_filename}')
                 except:
-                    print('error occurred while scraping the url')
-                else:
-                    try:
-                        filename = get_unique_filename('product_details.csv')
-                        df.to_csv(filename)
-                        print(f'product details saved to {filename}')
-                    except:
-                        print('error occurred while saving the file')
+                    print('Error occurred while scraping the URL')
         except TypeError or ValueError:
-            print('please enter a valid url')
+            print('Please enter a valid URL')
